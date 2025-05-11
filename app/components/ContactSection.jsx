7050e163bbadb7b4703cb6ddd742b059bf3991c3
Selvaganesh M <selvakvpvmk@gmail.com>
2025-05-11 17:46:07 +0530

    contact me content change

--- app/components/ContactSection.jsx
+++ app/components/ContactSection.jsx
@@ -6,7 +6,7 @@ const ContactSection = () => {
   return (
     <section id="contact" className="text-center py-8">
       <h2 className="text-4xl font-bold text-white m-8">
-        Contact me
+        Contact me...
       </h2>
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
 
